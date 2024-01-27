a51a55e9ad84919a120feef574c117f7563a8f0a
Khaled <khaledhadida_@hotmail.com>
2024-01-27 14:55:32 -0500

    All files were uploaded manually

---
+++ app/components/header.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react"
+import Link from 'next/link';
+
+export default function header() {
+
+  //useState
+  const [activeLink, setActiveLink] = useState<number | null>(null);
+
+  const handleLinkClick = (index: number) => {
+    setActiveLink(index);
+  };
+
+  return (
+    <div className="h-screen bg-black text-white">
+      <div className="flex justify-center items-center">
+        <nav className="bg-maroon p-5 m-3 space-x-4 rounded-full text-xl font-bold">
+          <Link href="/about"
+            className={`p-3 rounded-full ${activeLink === 0 ? 'bg-orange' : ''}`}
+            onClick={() => handleLinkClick(0)}>
+            About
+          </Link>
+
+          <Link href="/work"
+            className={`p-3 rounded-full ${activeLink === 1 ? 'bg-orange' : ''}`}
+            onClick={() => handleLinkClick(1)}>
+            Work
+          </Link>
+          <Link href="/projects"
+            className={`p-3 rounded-full ${activeLink === 2 ? 'bg-orange' : ''}`}
+            onClick={() => handleLinkClick(2)}>
+            Projects
+          </Link>
+      </nav>
+    </div>
+    </div >
+  )
+}
